fa8f872d5ef4f3fba006cb58ab824670f688a9b3
PeterDevLATAM <peter.devgrowth@gmail.com>
2022-04-15 10:10:14 -0500

    Boiler plate for categories reducer up to parent component of those who need that state

--- src/routes/shop/shop.component.jsx
+++ src/routes/shop/shop.component.jsx
@@ -1,9 +1,22 @@
 import { Routes, Route } from 'react-router-dom';
-
+import { useEffect } from 'react';
+import { getCategoriesAndDocuments } from '../../utils/firebase/firebase.utils';
 import CategoriesPreview from '../categories-preview/categories-preview.component';
 import Category from '../category/category.component';
+import { useDispatch } from 'react-redux';
+import { setCategoriesMap } from '../../store/category/categories.action';
 
 const Shop = () => {
+  const dispatch = useDispatch()
+  useEffect(() => {
+    const getCategoriesMap = async () => {
+      const categoryMap = await getCategoriesAndDocuments('categories');
+      //dispatch
+      dispatch(setCategoriesMap(categoryMap))
+    };
+
+    getCategoriesMap();
+  }, []);
   return (
     <Routes>
       <Route index element={<CategoriesPreview />} />
--- src/store/root-reducer.js
+++ src/store/root-reducer.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 
+import { categoriesReducer } from './category/categories.reducer';
 import { userReducer } from './user/user.reducer';
 
 export const rootReducer = combineReducers({
   //in here we use the reducer slice name and the actual reducer 
   user: userReducer,
+  categories: categoriesReducer,
 });
