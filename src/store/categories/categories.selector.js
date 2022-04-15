ba27f4cce12d7dd1450afb6d9444b8e7daac0f26
PeterDevLATAM <peter.devgrowth@gmail.com>
2022-04-15 15:34:11 -0500

    categories selector using reselect

--- src/store/categories/categories.selector.js
+++ src/store/categories/categories.selector.js
@@ -1,10 +1,28 @@
-export const selectCategoriesMap = (state) => {
-  const categoriesMap = state.categories.categories.reduce(
+import { createSelector } from "reselect";
+
+const selectCategoryReducer = (state) => {
+  return state.categories; //reducer
+};
+
+export const selectCategories = createSelector(
+  //input selector
+  [selectCategoryReducer],
+  //output selector
+  (categoriesReducerSliceState) => {
+    return categoriesReducerSliceState.categories;
+  }
+);
+// createSelector() creates a memoized selector, 
+// it doesn't fires unless the input selector changes
+
+export const selectCategoriesMap = createSelector(
+  [selectCategories],
+  (categories)=>{const categoriesMap = categories.reduce(
     (acc, { title, items }) => {
       acc[title.toLowerCase()] = items;
       return acc;
     },
     {}
   );
-  return categoriesMap;
-};
+  return categoriesMap;}
+)
