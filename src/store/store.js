6522248add8b6e3439f0c747b4e9ccc20214ad7d
PeterDevLATAM <peter.devgrowth@gmail.com>
2022-04-18 10:21:08 -0500

    add react persist library

--- src/store/store.js
+++ src/store/store.js
@@ -1,5 +1,7 @@
 import { compose, createStore, applyMiddleware } from 'redux';
 import logger from 'redux-logger';
+import {persistStore, persistReducer} from "redux-persist"
+import storage from "redux-persist/lib/storage"
 
 import { rootReducer } from './root-reducer';
 
@@ -7,6 +9,17 @@ const middleWares = [process.env.NODE_ENV === 'development' && logger].filter(
   Boolean
 );
 
+const persistConfig= {
+  key: "root", 
+  storage,
+  blacklist: ['users'] //in here you put the slice reducers you don't want to persist the state
+  // in this case firebase auth is taking care of the user so it may clash 
+
+}
+const persistedReducer = persistReducer(persistConfig, rootReducer)
+
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
-export const store = createStore(rootReducer, undefined, composedEnhancers);
+export const store = createStore(persistedReducer, undefined, composedEnhancers);
+
+export const persistor = persistStore(store)
